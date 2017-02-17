4d7e34f08ca0149e6e120cded2138e4801fe03d5
Johnny Hughes <johnnyhughes321@yahoo.com>
2017-02-17 18:00:54 +0000

    2_16 update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  resources :carts
+  resources :line_items
+  resources :cart
+  
+  get 'store/index'
+  
+  get 'static_pages/home'
+  
+  get 'static_pages/about'
+  
+  get 'static_pages/links'
+  
+  resources :store
   resources :products
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'store#index'
 end
+
